8f32c3e99e14312eb7dbb9a2492db094cc5afad6
glorydays <glorydays11@gmail.com>
2024-02-03 16:23:33 +0900

    add telescope

---
+++ lua/plugins/nvim/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('telescope').setup { -- Config is optional
+      vim.keymap.set('n', '<leader>ff', require 'telescope.builtin'.find_files, {}),
+      vim.keymap.set('n', '<leader>fg', require 'telescope.builtin'.live_grep, {}),
+      vim.keymap.set('n', '<leader>fb', require 'telescope.builtin'.buffers, {}),
+      vim.keymap.set('n', '<leader>fh', require 'telescope.builtin'.help_tags, {}),
+    }
+  end
+}
