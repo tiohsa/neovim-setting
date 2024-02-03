81f20bad0faf367216422792791f59339c797924
glorydays <glorydays11@gmail.com>
2024-02-03 16:27:43 +0900

    add telescope fzf

--- lua/plugins/nvim/telescope.lua
+++ lua/plugins/nvim/telescope.lua
@@ -1,7 +1,10 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.5',
-  dependencies = { 'nvim-lua/plenary.nvim' },
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    "nvim-telescope/telescope-fzf-native.nvim",
+  },
   config = function()
     require('telescope').setup { -- Config is optional
       vim.keymap.set('n', '<leader>ff', require 'telescope.builtin'.find_files, {}),
